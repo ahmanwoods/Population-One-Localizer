38cd3e5186da259c09469286e78a9be3c233d392
Ahman Woods <ahman.woods@gmail.com>
2023-05-31 15:17:06 -0600

    Update localizer

--- pop1_localizer.py
+++ pop1_localizer.py
@@ -5,32 +5,43 @@ from urllib.request import urlretrieve
 from tqdm import tqdm
 
 max_download_threads = 6
-build_number = 25402
+#prod for live, dev for playtest
+build_env = "prod"
+build_number = "v266"
 
 def get_urls():
-    with open('extract\catalog') as catalog_file:
+    json_files = [f for f in os.listdir('.') if f.endswith('.json')]
+    if len(json_files) != 1:
+        raise ValueError('multiple json files in the current directory')
+    global file
+    file = json_files[0]
+    with open(file) as catalog_file:
         urls = []
         catalog_json = json.load(catalog_file)
         for x in range(len(catalog_json['m_InternalIds'])):
             if "{BigBoxVR.UnityContentDeliveryManager.RemoteLoadPath}" in catalog_json['m_InternalIds'][x]:
-                urls.append(("https://appcdn.bigboxvr.com/prod/Build" + str(build_number) + "/StandaloneWindows64/" + catalog_json['m_InternalIds'][x][54:], "assets/" + catalog_json['m_InternalIds'][x][54:]))
+                urls.append(("https://appcdn.bigboxvr.com/PopOne/Assets/" + build_number + "/" + build_env + "/StandaloneWindows64/" + catalog_json['m_InternalIds'][x][54:], "assets/" + catalog_json['m_InternalIds'][x][54:]))
         return urls
 
 def create_catalog():
-    if not os.path.exists('replacement'):
-        os.makedirs('replacement')
+    if not os.path.exists('replacement_json'):
+        os.makedirs('replacement_json')
 
-    with open('extract\catalog') as catalog_file:
+    with open(file) as catalog_file:
         catalog_json = json.load(catalog_file)
         for x in range(len(catalog_json['m_InternalIds'])):
             if "{BigBoxVR.UnityContentDeliveryManager.RemoteLoadPath}" in catalog_json['m_InternalIds'][x]:
                 catalog_json['m_InternalIds'][x] = catalog_json['m_InternalIds'][x].replace("{BigBoxVR.UnityContentDeliveryManager.RemoteLoadPath}\\", "{UnityEngine.AddressableAssets.Addressables.RuntimePath}\\StandaloneWindows64\\")
     
-    with open('replacement\catalog', 'w') as output_json:
+    with open('replacement_json\\' + file, 'w') as output_json:
         json.dump(catalog_json, output_json)
 
 def urlretrieve_unpack(args):
-    return urlretrieve(*args)
+    try:
+        return urlretrieve(*args)
+    except Exception as e:
+        print(e)
+        return True
 
 if __name__ == '__main__':
     if not os.path.exists('assets'):
